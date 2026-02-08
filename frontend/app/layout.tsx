9c2e16bb71d9737626f87bb70cf26f436570b95c
sahith <129191778+sahith14@users.noreply.github.com>
2026-02-09 03:47:30 +0530

    Update layout.tsx

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next'
 import './globals.css'
-// import Navbar from '@/components/Navbar'
+import Navbar from '@/components/Navbar'
 import { inter } from './font'
 
 export const metadata: Metadata = {
@@ -16,6 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en" className={inter.variable}>
       <body className="min-h-screen bg-gradient-to-br from-gray-900 to-black">
+        <Navbar />
         {children}
       </body>
     </html>
